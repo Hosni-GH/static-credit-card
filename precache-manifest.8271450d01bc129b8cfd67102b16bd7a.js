617fdbb379675178bc9f198fb7c9e91b72b7e000
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-11-18 12:22:19 +0100

    Updates

---
+++ precache-manifest.8271450d01bc129b8cfd67102b16bd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2d11323620c879aa269878c2ceb45b",
+    "url": "/static-credit-card/index.html"
+  },
+  {
+    "revision": "66798b79f3da1203eaba",
+    "url": "/static-credit-card/static/css/main.99c988d7.chunk.css"
+  },
+  {
+    "revision": "0d06d2f61bb2dc92e6aa",
+    "url": "/static-credit-card/static/js/2.01d9c5fe.chunk.js"
+  },
+  {
+    "revision": "66798b79f3da1203eaba",
+    "url": "/static-credit-card/static/js/main.acb235f2.chunk.js"
+  },
+  {
+    "revision": "d6a0717341f6661fd5a0",
+    "url": "/static-credit-card/static/js/runtime-main.34ba59b2.js"
+  },
+  {
+    "revision": "8d796e3ec25a1750f17004c2d50d4b80",
+    "url": "/static-credit-card/static/media/terre-technologie.8d796e3e.jpg"
+  }
+]);
